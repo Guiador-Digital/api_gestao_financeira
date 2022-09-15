1b6a71426d185e0456d508ee22fee0fc627ab69f
Renan Joppert <renanljp@gmail.com>
2022-09-14 23:14:03 -0300

    #33 Criando nosso módulo de entradas

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\EntradaController;
 use App\Http\Controllers\UsuarioController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -21,3 +22,4 @@ use Illuminate\Support\Facades\Route;
 
 // Route::get('usuarios', [UsuarioController::class, 'index']);
 Route::resource('usuarios', UsuarioController::class);
+Route::resource('entradas', EntradaController::class);
