bbc9bd0daaba9cabeeebb989f21e81b567e8667c
Renan Joppert <renanljp@gmail.com>
2022-09-14 23:55:14 -0300

    #34 Criando nosso módulo de compras

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\CompraController;
 use App\Http\Controllers\EntradaController;
 use App\Http\Controllers\UsuarioController;
 use Illuminate\Http\Request;
@@ -23,3 +24,4 @@ use Illuminate\Support\Facades\Route;
 // Route::get('usuarios', [UsuarioController::class, 'index']);
 Route::resource('usuarios', UsuarioController::class);
 Route::resource('entradas', EntradaController::class);
+Route::resource('compras', CompraController::class);
